59c8f0bc1b260332d95f5980f3b7d1ce3fcb3e49
sebastian <sebastianglg64@gmail.com>
2022-10-19 11:11:18 -0500

    prueba de github - proyecto web daza

---
+++ JS/registro.js
@@ -0,0 +1,10 @@
+
+
+
+document.getElementById("button").addEventListener("click",function(){
+    document.querySelector(".popup").style.display = "flex";
+})
+
+document.querySelector(".close").addEventListener("click",function(){
+    document.querySelector(".popup").style.display = "none";
+})
